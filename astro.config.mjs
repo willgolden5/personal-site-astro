7a224a5849b266dc22426cdc61dc1d70681ef6c7
William Golden <wtgolden5@gmail.com>
2023-07-09 13:59:42 -0400

    remove netlify ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), image()],
-	output: 'server',
-	adapter: netlify(),
 });
